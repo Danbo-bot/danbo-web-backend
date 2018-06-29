edc27b50ab7a2f09b5efb38ac2f0ffc53a35c0f3
dashwav <dashwav@gmail.com>
2018-06-29 01:48:58 -0700

    Made logic a bit clearer

--- express/routes/servers.js
+++ express/routes/servers.js
@@ -9,32 +9,31 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:id', async function(req, res, next) {
-    Servers.find({
+    foundServer = await Servers.find({
         where:
         {
             server_id: req.params.id,
         }
     }).catch((err) => {
         console.error(err)}
-    ).then((foundServer) => {
-        if (!foundServer) { 
-            res.status(404).send('There isn\'t a server with that id in our database').end();
-            return; 
-        }
-        allUsers = await Users.findAll({
-            where: { server_id: foundServer.id},
-            order: [['experience', 'DESC']],
-        });
-        allRewards = await Rewards.findAll({
-            where: {server_id: foundServer.server_id},
-            order: [['level_attained', 'DESC']],
-        })
-        if (!allUsers) { res.status(500).send('Couldn\'t find users or rewards')}
-        res.status(200).send({
-            'id': foundServer.server_id,
-            'rewards': allRewards,
-            'users': allUsers
-        })
+    );
+    if (!foundServer) { 
+        res.status(404).send('There isn\'t a server with that id in our database').end();
+        return; 
+    }
+    allUsers = await Users.findAll({
+        where: { server_id: foundServer.id},
+        order: [['experience', 'DESC']],
+    });
+    allRewards = await Rewards.findAll({
+        where: {server_id: foundServer.server_id},
+        order: [['level_attained', 'DESC']],
+    })
+    if (!allUsers) { res.status(500).send('Couldn\'t find users or rewards')}
+    res.status(200).send({
+        'id': foundServer.server_id,
+        'rewards': allRewards,
+        'users': allUsers
     });
 });
 
